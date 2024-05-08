6073ec446f6eab503a8bc5a0386879fe15aa1412
Saurav4622 <paruisourabh4622@gmail.com>
2024-05-08 12:08:06 +0530

    build basic structure

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -1,8 +1,13 @@
 import random
 words = ['hello', 'another', 'world', 'geeks', 'new', 'other']
 word = random.choice(words)
-guess = input("Guess a word:")
-if guess == word:
-    print("You Won\nThe word is {}".format(word))
-else:
-    print("You Lose\nThe word is {}".format(word))
+turns = 5
+while turns >= 0:
+    guess = input("Guess a word:")
+    if guess == word:
+        print("You Won\nThe word is {}".format(word))
+        break
+    else:
+        print("You Lose\nTry Again ")
+        turns -= 1
+        print("You left with  {} chances".format(turns))
